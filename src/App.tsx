c2788edb2b2e743e90d9d48c5c2bf2df2d053ce4
mordmora <danielbonnett20@gmail.com>
2025-07-17 17:44:19 -0500

    Inicializacion del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '/vite.svg'
+import '@/styles/index.css'
+
+//Ejemplo
+
+function Greeting(){
+    return <>
+        <div>
+            "Hola mundo :)"
+        </div>
+    </>
+}
+
+function App() {
+    return Greeting();
+}
+
+export default App
