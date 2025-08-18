77ad45c995792486123518eb555b65b9fbb01486
Galoff <galofrexox@gmail.com>
2025-08-18 00:12:21 -0500

    Fixeando ServiceCard

--- src/dataServiceCard.tsx
+++ src/dataServiceCard.tsx
@@ -9,31 +9,31 @@ export const dataServiceCard= [
   },
   {
     id: "2",
-    icon: <span className="material-symbols-outlined">monitor</span>,
+    icon: <span className="material-symbols-outlined">mobile</span>,
     title: "Desarrollo de aplicaciones móviles",
     description: "Aplicaciones móviles nativas y multiplataforma para iOS y Android con una experiencia de usuario excepcional.",
   },
   {
     id: "3",
-    icon: <span className="material-symbols-outlined">monitor</span>,
+    icon: <span className="material-symbols-outlined">language</span>,
     title: "Desarrollo de plataformas web",
     description: "Aplicaciones web modernas y responsivas que utilizan tecnologías de vanguardia y las mejores prácticas.",
   },
    {
     id: "3",
-    icon: <span className="material-symbols-outlined">monitor</span>,
+    icon: <span className="material-symbols-outlined">database</span>,
     title: "Soluciones de bases de datos",
     description: "Diseño, optimización y gestión de bases de datos robustas para aplicaciones escalables basadas en datos.",
   },
    {
     id: "3",
-    icon: <span className="material-symbols-outlined">monitor</span>,
+    icon: <span className="material-symbols-outlined">dns</span>,
     title: "Ensamble y mantenimiento de equipos de computo",
     description: "Orientado a garantizar el rendimiento, estabilidad y prolongación de la vida útil de sus dispositivos.",
   },
    {
     id: "3",
-    icon: <span className="material-symbols-outlined">monitor</span>,
+    icon: <span className="material-symbols-outlined">shopping_bag</span>,
     title: "Venta de productos electronicos",
     description: "Amplia gama de productos electrónicos de alta calidad, ideales para el hogar, la oficina o entornos profesionales.",
   }
